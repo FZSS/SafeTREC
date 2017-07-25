464948bcbe0fae1c6c1cfec78f2b115bac0cd19a
Kevin Fangzhou Chen <kevin.cfz@berkeley.edu>
2017-07-25 15:34:42 -0700

    setup reducers and store

--- app/store/store.js
+++ app/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import reducer from '../reducers/reducer';
+import rootReducer from '../reducers/rootReducer';
 
 
 export default function configureStore(initialState) {
   return createStore(
-    reducer,
+    rootReducer,
     initialState,
   );
 }
